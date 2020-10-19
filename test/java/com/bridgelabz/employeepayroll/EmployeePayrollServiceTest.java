b646ef41efabd9da6e29106a92513e393c1267d6
sharale34 <sourabhharale8000@gmail.com>
2020-10-19 09:00:03 +0530

    Added: Ability for employee payroll service to print the employee payrolls

--- test/java/com/bridgelabz/employeepayroll/EmployeePayrollServiceTest.java
+++ test/java/com/bridgelabz/employeepayroll/EmployeePayrollServiceTest.java
@@ -20,4 +20,16 @@ public class EmployeePayrollServiceTest {
 		long entries = employeePayrollService.countEntries(IOService.FILE_IO);
 		Assert.assertEquals(3, entries);
 	}
+
+	@Test
+	public void writePayrollOnFile() {
+		EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+				new EmployeePayrollData(2, "Bill Gates", 200000.0),
+				new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0) };
+		EmployeePayrollService employeePayrollService;
+		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
+		long entries = employeePayrollService.countEntries(IOService.FILE_IO);
+		Assert.assertEquals(3, entries);
+	}
 }
